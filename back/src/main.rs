c3df63e05edd7f3918f2fdf4ba9a7a154350690c
Amaury Carrade <amaury@carrade.eu>
2020-12-11 10:24:30 +0100

    Added full support for /players
    - The endpoint returns players with the most recent activity (limited
      to 20 of them)
    - One can add ?filter=<?> to filter results by username
    - Non-players entries are not returned (for most of them)

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+#[macro_use] extern crate mysql;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 extern crate serde;
@@ -10,10 +11,9 @@ mod config;
 mod output;
 
 use figment::{Figment, providers::{Format, Toml, Env}};
+use mysql::prelude::*;
 use rocket::fairing::AdHoc;
 use rocket::State;
-use rocket_contrib::databases::mysql;
-use rocket_contrib::databases::mysql::prelude::*;
 use rocket_contrib::json::{Json, JsonValue};
 use uuid::Uuid;
 
@@ -43,8 +43,8 @@ fn index() -> &'static str {
         GET /ratio/<areas>/<uuids>
 
             Returns the ratio of the given player(s) in the given area(s).
-            `areas` are either `all` or a comma-separated list of areas.
-            `uuids` are a comma-separated list of UUIDs.
+            `areas` is either `all` or a comma-separated list of areas.
+            `uuids` is a comma-separated list of UUIDs.
             Data will be aggregated as a whole from all areas and all players.
 
             Results are cached. Cache information will be available in the response. To force
@@ -64,9 +64,49 @@ fn areas(areas: State<Areas>) -> Json<Areas>{
 #[get("/players?<filter>")]
 async fn players(filter: Option<String>, db: PrismDatabase) -> Result<Json<Vec<Player>>, Json<JsonValue>> {
     let players = db.run(|c: &mut mysql::Conn| {
-        c.query_map(
-            "SELECT player AS name, HEX(player_uuid) as uuid FROM prism_players LIMIT 10",
-            |(name, uuid): (String, String)| Player { name, uuid: Uuid::parse_str(uuid.as_str()).unwrap_or(Uuid::nil()) }
+        let stmt = c.prep("
+            SELECT
+                p.player AS name,
+                HEX(p.player_uuid) AS uuid,
+                (
+                   SELECT epoch
+                   FROM prism_data d
+                   WHERE d.player_id = p.player_id
+                   ORDER BY epoch DESC
+                   LIMIT 1
+                ) AS last_action
+            FROM prism_players p
+            WHERE
+                p.player LIKE :player
+                -- We try to exclude non-player entries
+                AND NOT p.player LIKE '%:%'
+                AND NOT p.player LIKE '% %'
+                AND p.player NOT IN (
+                    'Piston', 'custom', 'zombie', 'skeleton', 'Lava',
+                    'dispenser', 'beehive', 'Environment', 'suffocation',
+                    'mount', 'spawner', 'fall', 'water', 'player', 'cramming',
+                    'breeding', 'creeper', 'guardian', 'drowning', 'drowned',
+                    'fire', 'piglin', 'unknown', 'default', 'tnt', 'witch',
+                    'villager', 'patrol', 'lightning', 'shulker', 'pillager',
+                    'dryout', 'egg', 'wither_skeleton', 'ocelot', 'fireball',
+                    'infection', 'player_unleash', 'holder_gone', 'blaze',
+                    'enderman', 'spectral_arrow', 'piglin_brute', 'hoglin',
+                    'strider', 'vex', 'vindicator', 'raid', 'wolf', 'stray',
+                    'husk', 'distance', 'turtle', 'wither', 'zombie_villager',
+                    'wandering_trader', 'arrow', 'cured', 'void', 'trap',
+                    'jockey', 'spider', 'snowman', 'starvation', 'sheep', 'cow',
+                    'trader_llama', 'fox', 'magma_cube', 'horse', 'projectile',
+                    'rabbit', 'parrot', 'donkey', 'cat', 'skeleton_horse',
+                    'chicken', 'zombified_piglin', 'evoker', 'ravager', 'ghast',
+                    'endermite'
+                )
+            ORDER BY last_action DESC
+            LIMIT 20
+        ")?;
+        let player = format!("%{}%", filter.unwrap_or(String::from("")));
+        c.exec_map(
+            stmt, params! { player },
+            |(name, uuid, _): (String, String, u64)| Player { name, uuid: Uuid::parse_str(uuid.as_str()).unwrap_or(Uuid::nil()) }
         )
     }).await;
 
