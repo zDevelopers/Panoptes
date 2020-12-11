6bf5b7f7ab7b47312fbff3f679fd2743434fe409
Amaury Carrade <amaury@carrade.eu>
2020-12-11 12:41:10 +0100

    CORS support (no config yet)

--- back/src/config.rs
+++ back/src/config.rs
@@ -1,15 +1,28 @@
 use std::collections::HashMap;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     pub areas: HashMap<String, ConfigArea>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct ConfigArea {
     pub name: String,
     pub world: String,
     pub pos1: Vec<i64>,
     pub pos2: Vec<i64>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CorsConfig {
+    pub cors: String
+}
+
+impl Default for CorsConfig {
+    fn default() -> CorsConfig {
+        CorsConfig {
+            cors: String::from("*")
+        }
+    }
 }
--- back/src/main.rs
+++ back/src/main.rs
@@ -10,15 +10,17 @@ mod area;
 mod config;
 mod output;
 
-use figment::{Figment, providers::{Format, Toml, Env}};
+use figment::{Figment, providers::{Env, Format, Serialized, Toml}};
 use mysql::prelude::*;
 use rocket::fairing::AdHoc;
+use rocket::http::Header;
 use rocket::State;
+use rocket_contrib::helmet::SpaceHelmet;
 use rocket_contrib::json::{Json, JsonValue};
 use uuid::Uuid;
 
 use crate::area::Areas;
-use crate::config::Config;
+use crate::config::{Config, CorsConfig};
 use crate::output::Player;
 
 
@@ -120,18 +122,33 @@ async fn players(filter: Option<String>, db: PrismDatabase) -> Result<Json<Vec<P
 #[launch]
 fn rocket() -> rocket::Rocket {
     let figment = rocket::Config::figment()
+        .merge(Serialized::defaults(CorsConfig::default()))
         .merge(Toml::file("Panoptes.toml").nested())
         .merge(Toml::file(Env::var_or("PANOPTES_CONFIG", "../Panoptes.toml")).nested())
         .merge(Env::prefixed("PANOPTES_").global());
+
+    let cors_config: CorsConfig = figment.extract().expect("Missing or invalid CORS configuration");
+    let cors = cors_config.cors;
 
     rocket::custom(figment)
         .mount("/", routes![index, areas, players])
         .attach(AdHoc::on_attach("Areas Configuration", |rocket| async {
             let figment: &Figment = rocket.figment();
-            let config: Config = figment.extract().expect("Missing or invalid areas configuration");
+            let config: Config = match figment.extract() {
+                Ok(config) => config,
+                Err(e) => {
+                    rocket::config::pretty_print_error(e);
+                    return Err(rocket);
+                }
+            };
+
             let areas: Areas = config.into();
 
             Ok(rocket.manage(areas))
         }))
         .attach(PrismDatabase::fairing())
+        .attach(SpaceHelmet::default())
+        .attach(AdHoc::on_response("CORS header", |_, res| Box::pin(async move {
+            res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        })))
 }
